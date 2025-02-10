0106eb807282f2561c0c78e38ec231c646f6bc02
codeza-ai <darshan.odedara@gmail.com>
2025-02-10 22:59:50 +0530

    Initialized the frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+// import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello World</h1>
+    </>
+  )
+}
+
+export default App
